e9d4bf5688e047cbda50beb158b7174e327f1d62
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-10 18:41:05 -0500

    Se le añadio la validación de los errores de los input,los cuales son required, min, max, maxLenght, minLenght

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,33 +26,113 @@ export const Form = () => {
                     className="form-control"
                     type="text"
                     placeholder="title"
-                    {...register("title")}
+                    {...register("title", {
+                      required: true,
+                      maxLength: 20,
+                      minLength: 2,
+                    })}
                   />
+                  {errors.title?.type === "required" && (
+                    <small className="text-danger">
+                      El titulo es requerido
+                    </small>
+                  )}
+                  {errors.title?.type === "maxLength" && (
+                    <small className="text-danger">
+                      El titulo debe tener maximo 20 caracteres
+                    </small>
+                  )}
+                  {errors.title?.type === "minLength" && (
+                    <small className="text-danger">
+                      El titulo debe tener minimo 2 caracteres
+                    </small>
+                  )}
                 </div>
+
                 <div className="mb-3">
                   <input
                     className="form-control"
                     type="number"
                     placeholder="price"
-                    {...register("price")}
+                    {...register("price", {
+                      required: true,
+                      min: 10,
+                      max: 1000,
+                    })}
                   />
+                  {errors.price?.type === "required" && (
+                    <small className="text-danger">
+                      El precio es requerido
+                    </small>
+                  )}
+                  {errors.price?.type === "max" && (
+                    <small className="text-danger">
+                      El precio maximo es de 1,000
+                    </small>
+                  )}
+                  {errors.price?.type === "min" && (
+                    <small className="text-danger">
+                      El precio minimo es de 10
+                    </small>
+                  )}
                 </div>
+
                 <div className="mb-3">
                   <textarea
                     className="form-control"
                     rows="2"
                     placeholder="description"
-                    {...register("description")}
+                    {...register("description", {
+                      required: true,
+                      maxLength: 50,
+                      minLength: 10,
+                    })}
                   ></textarea>
+                  {errors.description?.type === "required" && (
+                    <small className="text-danger">
+                      La descripcion es requerida
+                    </small>
+                  )}
+                  {errors.description?.type === "maxLength" && (
+                    <small className="text-danger">
+                      La descripcion debe tener maximo 50 caracteres
+                    </small>
+                  )}
+                  {errors.description?.type === "minLength" && (
+                    <small className="text-danger">
+                      La descripcion debe tener maximo 10 caracteres
+                    </small>
+                  )}
                 </div>
+
                 <div className="mb-3">
                   <input
                     className="form-control"
                     type="text"
                     placeholder="category"
-                    {...register("category")}
+                    {...register("category", {
+                      required: true,
+                      maxLength: 20,
+                      minLength: 5,
+                    })}
                   />
+                  {errors.category?.type === "required" && (
+                    <small className="text-danger">
+                      La categoria es requerida
+                    </small>
+                  )}
+                  {errors.category?.type === "maxLength" && (
+                    <small className="text-danger">
+                      La categoria debe tener maximo 20 caracteres
+                    </small>
+                  )}
+                  {errors.category?.type === "minLength" && (
+                    <small className="text-danger">
+                      La categoria debe tener minimo 5 caracteres
+                    </small>
+                  )}
                 </div>
+
                 <div className="mb-3 d-grid gap-2">
                   <button className="btn btn-primary" type="submit">
                     Save
